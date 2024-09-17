d2149d3a11385b081bf294c5395ccc9adfdd759f
Edirisinghe <hasithale@gmail.com>
2024-09-17 23:39:42 +0530

    Add project files.

---
+++ DevBlog.Web/Data/DevBlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DevBlog.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevBlog.Web.Data
+{
+    public class DevBlogDbContext : DbContext
+    {
+        public DevBlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
