febb62ecf44c97ad4ca0c0cecc9798661d47d7dd
Edirisinghe <hasithale@gmail.com>
2024-09-21 16:23:27 +0530

    feature: add new blog form created

--- DevBlog.Web/Pages/Admin/Blogs/Add.cshtml.cs
+++ DevBlog.Web/Pages/Admin/Blogs/Add.cshtml.cs
@@ -1,3 +1,6 @@
+using DevBlog.Web.Data;
+using DevBlog.Web.Models.Domain;
+using DevBlog.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,34 @@ namespace DevBlog.Web.Pages.Admin.Blogs
 {
     public class AddModel : PageModel
     {
+        private readonly DevBlogDbContext devBlogDbContext;
+
+        [BindProperty]
+        public AddBlog AddBlogPostRequest { get; set; }
+        public AddModel(DevBlogDbContext devBlogDbContext)
+        {
+            this.devBlogDbContext = devBlogDbContext;
+        }
         public void OnGet()
         {
+        }
+
+        public void OnPost() 
+        {
+            var blogPost = new BlogPost
+            {
+                Heading = AddBlogPostRequest.Heading,
+                PageTitle = AddBlogPostRequest.PageTitle,
+                Content = AddBlogPostRequest.Content,
+                ShortDescription = AddBlogPostRequest.ShortDescription,
+                FeaturedImageUrl = AddBlogPostRequest.FeaturedImageUrl,
+                UrlHandle = AddBlogPostRequest.UrlHandle,
+                PublishedDate = AddBlogPostRequest.PublishedDate,
+                Author = AddBlogPostRequest.Author,
+                Visible = AddBlogPostRequest.Visible
+            };
+            devBlogDbContext.Add(blogPost);
+            devBlogDbContext.SaveChanges();
         }
     }
 }
